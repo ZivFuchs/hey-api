44202867e4b87a2ead71d26f52e55c7323dabcae
ZivFuchs <ziv.fuchs@gmail.com>
2026-06-24 11:22:27 -0700

    chore: added reproduction example

---
+++ examples/bug-tanstack-query-sdk-flat-params-integration/openapi-ts.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, type UserConfig } from '@hey-api/openapi-ts';
+
+const config: Promise<UserConfig> = defineConfig({
+  input:
+    'https://raw.githubusercontent.com/swagger-api/swagger-petstore/master/src/main/resources/openapi.yaml',
+  logs: {
+    path: './logs',
+  },
+  output: {
+    path: './src/client',
+    postProcess: ['oxfmt', 'eslint'],
+  },
+  plugins: [
+    '@hey-api/client-fetch',
+    '@hey-api/schemas',
+    {
+      instance: true,
+      name: '@hey-api/sdk',
+      paramsStructure: 'flat',
+    },
+    {
+      enums: 'javascript',
+      name: '@hey-api/typescript',
+    },
+    '@tanstack/react-query',
+  ],
+});
+
+export default config;
